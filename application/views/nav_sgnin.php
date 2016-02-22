7676b0a9641f7028b4ee5291b7f6db7d2ad2c9fd
Eli Byers <elibyers.dev@gmail.com>
2016-02-22 15:54:19 -0800

    Minor changes to formating
    I reordered some input fields, changed some routes, and removed a
    helper file I realized would not work.

--- application/views/nav_sgnin.php
+++ application/views/nav_sgnin.php
@@ -5,13 +5,13 @@
 				<p class="navbar-brand">Test App</p>
 			</div>
 			<ul class="nav navbar-nav navbar-left">
-				<li><a href="dashboard">Dashboard</a></li>
+				<li><a href="/dashboard/admin">Dashboard</a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-left">
-				<li><a href="profile">Profile</a></li>
+				<li><a href="profile<?= $this->session->userdata('user_id') ?>">Profile</a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="login">Log Off</a></li>
+				<li><a href="/logoff">Log Off</a></li>
 			</ul>
 		</div>
 	</nav>
--- application/views/new_user.php
+++ application/views/new_user.php
@@ -6,7 +6,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Register</title>
+	<title>New User</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
@@ -16,19 +16,21 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-	<link rel="stylesheet" type="text/css" href="application/assets/login_style.css">
+	<link rel="stylesheet" type="text/css" href="/application/assets/login_style.css">
 </head>
 <body>
 	<div class="container">
 
+		<a href="dashboard/admin" class='btn btn-primary'>Return to Dashboard</a>
+
 		<div class='in_box panel panel-default col-md-6 col-md-offset-3'>
 			<h3 class='row col-xs-12'>
-				Register
+				Add a new user
 				<?php if($this->session->flashdata("reg_error")){
 					echo "<span class='error'>Invalid registration data</span>";
 				}?>
 			</h3>
-			<form action="register_user" method="post">
+			<form action="users/new" method="post">
 				<div class='row form-group'>
 					<div class='col-xs-12'>
 						<label>Email:</label>
@@ -60,11 +62,9 @@
 					</div>
 				</div>
 				<div class='row form-group'>
-					<input class='btn btn-success pull-right' type="submit" value="Register" role='button'/>
+					<input class='btn btn-success pull-right' type="submit" value="Create" role='button'/>
 				</div>
 			</form>
-
-			<a href="login" class='reg'>Already have an account? Log In!</a>
 		</div>
 
 	</div> <!-- end of container -->
